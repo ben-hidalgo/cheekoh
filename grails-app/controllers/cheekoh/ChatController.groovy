2a9e4fdc00f056309de01e1a65ce529544426fab
Ben Hidalgo <ben.hidalgo@gmail.com>
2012-09-10 18:29:31 -0700

    added simplistic chat

--- grails-app/controllers/cheekoh/ChatController.groovy
+++ grails-app/controllers/cheekoh/ChatController.groovy
@@ -6,6 +6,23 @@ class ChatController {
     
     def submit() {
     
-        sendMessage('seda:inboundChat', 'hi')
+       def builder = new groovy.json.JsonBuilder()
+       def root = builder.people {
+           person {
+               firstName new Date()
+               lastName params.comment
+               // Named arguments are valid values for objects too
+               address(
+                       city: 'Paris',
+                       country: 'France',
+                       zip: 12345,
+               )
+               married true
+               // a list of values
+               conferences 'JavaOne', 'Gr8conf'
+           }
+       }
+       
+        sendMessage('seda:inboundChat', builder.toString())
     }
 }
